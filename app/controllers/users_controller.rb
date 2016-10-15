31449156187a68a2b3455aa44fd24f74a10de343
hidekishibuya <shibuyahideki@gmail.com>
2016-10-15 06:41:30 +0000

    ユーザーインデックス追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :followings, :followers]
   before_action :correct_user, only: [:edit, :update]
   
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
@@ -33,10 +37,6 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
-  end
-  
-  def index
-    @users = User.all
   end
   
   def followings
