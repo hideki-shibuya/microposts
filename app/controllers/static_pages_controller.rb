a242e93b42a73d50ceacbd2eb9bff97f6309a906
hidekishibuya <shibuyahideki@gmail.com>
2016-10-19 13:21:24 +0000

    ユーザーページ更新

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
-      @feed_items = current_user.feed_items.paginate(page: params[:page])
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).paginate(page: params[:page])
     end
   end
 end
